986572e24d72d7fc00b928313f34e95c8bd43215
Tushaar Kamat <platypusdiamond@gmail.com>
2018-03-18 20:10:56 -0400

    Finished core functions, wrote function that advances generation

--- src/game_of_life/life.clj
+++ src/game_of_life/life.clj
@@ -2,7 +2,110 @@
   (:require [quil.core :as quil]))
 
 (defn create-grid
-  "Creates a 2D grid of the specified dimensions"
+  "Returns a 2D grid of the specified dimensions"
   [x y]
   (repeat y (repeat x ".")))
+
+(defn assoc-live-cell
+  "Returns a new grid with the specified live cell"
+  [cell grid]
+  (assoc-in grid cell "X"))
+
+(defn assoc-dead-cell
+  "Returns a new grid with the specified dead cell"
+  [cell grid]
+  (assoc-in grid cell "."))
+
+(defn is-alive?
+  "Returns true if the specified cell of the grid contains an alive cell,
+  false if otherwise"
+  [cell grid]
+  (if (= (get-in grid cell) "X")
+    true
+    false))
+
+(def neighbor-directions
+  "A set of all of the possible directions to move"
+  (let [directions (range -1 2)]
+    (for [x directions
+          y directions
+          :when (not (and (= x 0) (= y 0)))]
+      [x y])))
+
+(defn get-neighbors
+  "Returns a set of the orthagonal and diagonal neighbors of the given cell"
+  [[x y :as cell] grid]
+  (->> neighbor-directions
+       (map (fn [[xd yd]] [(+ x xd) (+ y yd)]))
+       (filter #(get-in grid %))
+       set))
+
+(defn count-live-neighbors
+  "Returns a set of live neighbors of a given cell"
+  [cell grid]
+  (count (filter #(is-alive? % grid) (get-neighbors cell grid))))
+
+(defn create-cell-list
+  "Creates a list of cell coordinates from the grid"
+  [grid]
+  (set (for [x (range 0 (count grid))
+             y (range 0 (count grid))]
+         [x y])))
+
+(defn kill-underpopulated-cells
+  "Removes any live cells from the grid that have less than 2 living neighbors,
+  and returns the changes in the grid-state map"
+  [grid grid-state]
+  (let [cell-list (create-cell-list grid)]
+    (reduce (fn [grid-state cell]
+              (if (and (is-alive? cell grid)
+                       (< (count-live-neighbors cell grid) 2))
+                (update grid-state :dead #(conj % cell))
+                grid-state))
+            grid-state
+            cell-list)))
+
+(defn kill-overpopulated-cells
+  "Removes any live cells from the grid that have more than 3 living neighbors,
+  and returns the changes in the grid-state map"
+  [grid grid-state]
+  (let [cell-list (create-cell-list grid)]
+    (reduce (fn [grid-state cell]
+              (if (and (is-alive? cell grid)
+                       (> (count-live-neighbors cell grid) 3))
+                (update grid-state :dead #(conj % cell))
+                grid-state))
+            grid-state
+            cell-list)))
+
+(defn reproduce-empty-cells
+  "Enlivens any dead cells from the grid that have exactly 3 live neighbors, and
+  returns the changes in the grid-state map"
+  [grid grid-state]
+  (let [cell-list (create-cell-list grid)]
+    (reduce (fn [grid-state cell]
+              (if (and (not (is-alive? cell grid))
+                       (= (count-live-neighbors cell grid) 3))
+                (update grid-state :alive #(conj % cell))
+                grid-state))
+            grid-state
+            cell-list)))
+
+(defn apply-grid-functions
+  [grid]
+  (->> {:dead #{} :alive #{}}
+                        (kill-underpopulated-cells grid)
+                        (kill-overpopulated-cells grid)
+                        (reproduce-empty-cells grid)))
+
+(defn step-through-generation
+  "Steps through a single generation by applying the killing and reproduction
+  functions to the grid, returns a new grid"
+  [grid]
+  (let [grid-state (apply-grid-functions grid)]
+    (reduce (fn [grid dead-cell] (assoc-dead-cell dead-cell grid))
+     (reduce (fn [grid alive-cell] (assoc-live-cell alive-cell grid))
+                    grid
+                    (:alive grid-state))
+     (:dead grid-state))))
 
--- test/game_of_life/life_test.clj
+++ test/game_of_life/life_test.clj
@@ -2,6 +2,59 @@
   (:require [clojure.test :refer :all]
             [game-of-life.life :refer :all]))
 
+(def mock-grid-empty [["." "." "."] ["." "." "."] ["." "." "."]])
+(def mock-grid-blinker [["." "X" "."] ["." "X" "."] ["." "X" "."]])
+(def mock-grid-star [["." "X" "."] ["X" "X" "X"] ["." "X" "."]])
+
 (deftest create-grid-test
-  (is (= (create-grid 2 2) [["." "."] ["." "."]])))
+  (is (= mock-grid-empty (create-grid 3 3))))
+
+(deftest assoc-live-cell-test
+  (is (= [["X" "." "."] ["." "." "."] ["." "." "."]]
+         (assoc-live-cell [0 0] mock-grid-empty))))
+
+(deftest assoc-dead-cell-test
+  (is (= [["." "." "."] ["." "X" "."] ["." "X" "."]]
+         (assoc-dead-cell [0 1] mock-grid-blinker))))
+
+(deftest is-alive-test
+  (is (= true (is-alive? [0 1] mock-grid-blinker))))
+
+(deftest get-neighbors-test
+  (is (= #{[0 0] [0 1] [0 2] [1 0] [1 2] [2 0] [2 1] [2 2]}
+         (get-neighbors [1 1] mock-grid-blinker))))
+
+(deftest get-neighbors-in-corner-test
+  (is (= #{[0 1] [1 1] [1 0]}
+         (get-neighbors [0 0] mock-grid-blinker))))
+
+(deftest count-live-neighbors-test
+  (is (= 2 (count-live-neighbors [1 1] mock-grid-blinker))))
+
+(deftest kill-underpopulated-cells-test
+  (is (= {:dead #{[0 1] [2 1]} :alive #{}}
+         (kill-underpopulated-cells mock-grid-blinker {:alive #{} :dead #{}}))))
+
+(deftest kill-overpopulated-cells-test
+  (is (= {:dead #{[1 1]} :alive #{}}
+         (kill-overpopulated-cells mock-grid-star {:alive #{} :dead #{}}))))
+
+(deftest reproduce-empty-cells-test
+  (is (= {:alive #{[1 0] [1 2]} :dead #{}}
+         (reproduce-empty-cells mock-grid-blinker {:alive #{} :dead #{}}))))
+
+(deftest step-through-generation-blinker-test
+  (is (= [["." "." "."] ["X" "X" "X"] ["." "." "."]]
+         (step-through-generation mock-grid-blinker))))
+
+(deftest step-through-generation-star-test
+  (is (= [["X" "X" "X"] ["X" "." "X"] ["X" "X" "X"]]
+         (step-through-generation mock-grid-star))))
+
+
+
+
+
+
+
 
